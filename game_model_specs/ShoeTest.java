1ebfbb034a7286cc0a280068c4b04249591ca91c
Glen Young <extractofcactus@icloud.com>
2017-07-07 14:11:53 +0100

    Shoe has 52 cards

--- game_model_specs/ShoeTest.java
+++ game_model_specs/ShoeTest.java
@@ -1,12 +1,23 @@
-// import static org.junit.Assert.*;
-// import org.junit.*;
-// import game_models.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+import game_models.*;
+import enums.*;
 
-// public class ShoeTest {
-//   Shoe shoe;
+public class ShoeTest {
+  Shoe shoe;
 
-//   @Before
-//   public void before() {
-//     shoe = new Shoe();
-//   }
-// }
+  @Before
+  public void before() {
+    shoe = new Shoe();
+  }
+
+  @Test
+  public void shoeHasCards() {
+    assertEquals(52, shoe.size());
+  }
+
+  // @Test
+  //   public void canGetCardByIndex() {
+  //     assertEquals(Suit.CLUBS, shoe.getCardByIndex(16).getSuit());
+  //   }
+}
--- game_models/Shoe.java
+++ game_models/Shoe.java
@@ -1,5 +1,6 @@
 package game_models;
 import java.util.ArrayList;
+import enums.*;
 
 public class Shoe {
   ArrayList<Card> deck;
@@ -7,6 +8,19 @@ public class Shoe {
 
   public Shoe() {
     this.deck = new ArrayList<Card>();
-    // this.setupDeck();
+    this.setUpDeck();
+  }
+
+  public void setUpDeck() {
+    for (Rank rank : Rank.values()) {
+      for (Suit suit : Suit.values()) {
+        Card card = new Card(rank, suit);
+        this.deck.add(card);
+      }
+    }
+  }
+
+  public int size() {
+    return this.deck.size();
   }
 }
